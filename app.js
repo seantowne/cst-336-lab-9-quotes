b248faa4e8ad224f1a49a4fc3e9031d5f57f830d
seantowne <stowne@csumb.edu>
2020-05-02 05:04:59 +0000

    AutomatedCommitAndPush

--- app.js
+++ app.js
@@ -11,25 +11,46 @@ var mysql = require('mysql');
 const production_environment = process.env.PRODUCTION;
 console.log("Production="+production_environment);
 
-var connection;
-
+var dbConfig;
 if ( process.env.PRODUCTION == "TRUE"){
-    connection = mysql.createConnection({
+    dbConfig = {
         host: 'us-cdbr-east-06.cleardb.net',
         user: 'b22500485272d5',
         password: 'b511a945',
-        database: 'heroku_66ec8100dd2a87c'
-    });
+        database: 'heroku_66ec8100dd2a87c',
+        port: 3306
+    };
 }
 else{
-    connection = mysql.createConnection({
+    dbConfig = {
         host: 'localhost',
         user: 'seantowne',
         password: 'password',
-        database: 'Lab9'
+        database: 'Lab9',
+        port: 3306
+    };
+}
+
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect( function onConnect(err) {   // The server is either down
+        if (err) {                                  // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
+        console.log('db error', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
     });
 }
-connection.connect();
+handleDisconnect();
 
 
 app.set('view engine', 'ejs');
